5c99a63b4755ff23a9b0f1398d36316c4c15bd14
Phuc Nguyen <ph.nguyen@linkbynet.com>
2016-12-01 17:50:22 +0700

    Register asset file

--- src/EventCalendar.php
+++ src/EventCalendar.php
@@ -2,11 +2,17 @@
 
 namespace phucnguyenvn\yii2evecalendar;
 
-class Fullcalendar extends \edofre\fullcalendar\Fullcalendar
+class EventCalendar extends \edofre\fullcalendar\Fullcalendar
 {
   public $header = [
 		'center' => 'title',
 		'left'   => 'prev,next, today',
 		'right'  => 'listYear,month,agendaWeek,agendaDay'
 	];
+	
+  public function run()
+  {
+    assets\ModuleAsset::register($this->view);
+    return parent::run();
+  }
 }
